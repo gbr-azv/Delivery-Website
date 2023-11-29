a4fb79efccc0f527c0e30e42cc0dc9dbe5720b7f
Gabriel Azevedo <gabrielp.azevedo@outlook.com>
2023-11-29 14:25:10 -0300

    feat: setting new paths

--- app/main.py
+++ app/main.py
@@ -1,12 +1,51 @@
-from typing import Union
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello":"World"}
+class Menu(BaseModel):
+    Main: str
+    Drink: str
+    Dessert: str
+    Additional: bool = False
+    Spicy: Optional[int] = None
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# Remaining Cart's Selection
+cache = [{"Main":"X-Salad", "Drink": "Soda", "Dessert":"Ice Cream", "Additional":True, "Spicy":5, "id":12345},
+      {"Main":"Chicken Burguer", "Drink": "Milk", "Dessert":"Milkshake", "Additional":True, "Spicy":10, "id":13567}]
+
+# Menu
+menu = [{"Main":["X-Salad", "Chicken Burguer", "Double Cheese Burguer"]},
+        {"Drinks":["Soda", "Milk", "Orange Juice"]},
+        {"Desserts":["Ice Cream", "Milkshake", "Donuts"]}]
+
+# Root
+@app.get("/")
+def home():
+    return {"Message":"Welcome to Joe's Restaurant Menu"}
+
+# Menu
+@app.get("/menu")
+def get_menu():
+    return{"Menu":menu}
+
+# Add To Cart
+@app.post("/cart")
+def post_cart(cart: Menu):
+    cart_dict = cart.model_dump()
+    cart_dict["id"] = randrange(0,99999)
+    cache.append(cart_dict)
+    return{"Cart":cart_dict}
+
+# Show Current Cart
+@app.get("/cart")
+def get_cart():
+    return{"Cart":cache}
+
+# Show specific order
+@app.get("/cart/{id}")
+def get_order(id):
+    return {id:"Here is your order"}
