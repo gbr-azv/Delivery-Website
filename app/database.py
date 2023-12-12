b83e601c22866e04c9c23a76f39b313e2de56380
Gabriel Azevedo <gabrielp.azevedo@outlook.com>
2023-12-12 17:48:09 -0300

    config: define ports for database

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:e3rxapap@postgres/FastAPI'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:e3rxapap@postgres:5432/FastAPI'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
